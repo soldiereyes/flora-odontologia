6664f56ac1b396e339efbdbce6fb59512fac21ac
Guilherme Gonzaga <guilherme.gonzaga@eprecise.com.br>
2023-06-03 15:35:23 -0300

    feature: remoção da navbar no main.

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import Navbar from "./componentes/navbar/Navbar.tsx";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <Navbar/>
         <App/>
     </React.StrictMode>,
 )
