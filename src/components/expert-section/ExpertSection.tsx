68eccbf1691cb33055b3e6418c1038117c5086db
Guilherme Gonzaga <guilherme.gonzaga@eprecise.com.br>
2023-06-14 19:29:39 -0300

    fix: alteração no grupo de fonte(ainda não definitivo).

--- src/components/expert-section/ExpertSection.tsx
+++ src/components/expert-section/ExpertSection.tsx
@@ -1,22 +1,48 @@
-import React from 'react';
+
+import React, { useEffect, useRef, useState } from 'react';
 import './ExpertSection.css';
 
 interface SectionProps {
     imageUrl: string;
     text: string;
     description: string;
+    cro?: string;
 }
 
-const ExpertSection: React.FC<SectionProps> = ({imageUrl, text, description}) => {
+const ExpertSection: React.FC<SectionProps> = ({imageUrl, text, description,cro}) => {
+
+    const [isVisible, setIsVisible] = useState(false);
+    const componentRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const checkVisibility = () => {
+            if (componentRef.current) {
+                const rect = componentRef.current.getBoundingClientRect();
+                const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+                setIsVisible(rect.top <= windowHeight && rect.bottom >= 0);
+            }
+        };
+
+        window.addEventListener('scroll', checkVisibility);
+        checkVisibility();
+
+        return () => {
+            window.removeEventListener('scroll', checkVisibility);
+        };
+    }, []);
+
+
+
     return (
         <section className="section-expert-section">
             <div className="column-1">
                 <div className="card-inner-expert-section">
-                    <div className="content-name-image">
+                    <div className={`content-name-image ${isVisible ? 'visible' : ''}`} ref={componentRef}>
                         <div className="circle">
                             <img src={imageUrl} alt="Imagem" className="image"/>
                         </div>
                         <div className='description-expert-section'>{description}</div>
+                        <div className='description-expert-section-cro'>{cro}</div>
                     </div>
                 </div>
             </div>
