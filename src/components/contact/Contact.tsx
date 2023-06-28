85ccaae67e2ccb9706359f24dc840d8b56da3135
Guilherme Gonzaga <guilherme.gonzaga@eprecise.com.br>
2023-06-28 20:24:12 -0300

    feature: implementação de animações e links para mídias sociais.

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Contact.css';
 
-import { BsTelephone } from "react-icons/bs";
-import { FiSmartphone } from "react-icons/fi";
-import { FiMail } from "react-icons/fi";
-import { BsInstagram } from "react-icons/bs";
+import {BsTelephone} from "react-icons/bs";
+import {FiSmartphone} from "react-icons/fi";
+import {FiMail} from "react-icons/fi";
+import {BsInstagram} from "react-icons/bs";
 
 interface SectionProps {
     phone1: string;
@@ -14,39 +14,43 @@ interface SectionProps {
     imageUrl: string;
 }
 
-const Contact: React.FC<SectionProps> = ({ phone1, phone2, email, instagram, imageUrl }) => {
+const Contact: React.FC<SectionProps> = ({phone1, phone2, email, instagram, imageUrl}) => {
     return (
         <section className="section-contact">
             <div className="column-contact">
                 <div className="card">
                     <div className="contact-field">
                         <div className="icon">
-                            <BsTelephone />
+                            <BsTelephone/>
                         </div>
                         <div className="contact-info">{phone1}</div>
                     </div>
                     <div className="contact-field">
                         <div className="icon">
-                            <FiSmartphone />
+                            <FiSmartphone/>
                         </div>
-                        <div className="contact-info">{phone2}</div>
+                        <a href="https://wa.me/5546998775560">
+                            <div className="contact-info">{phone2}</div>
+                        </a>
                     </div>
                     <div className="contact-field">
                         <div className="icon">
-                            <FiMail />
+                            <FiMail/>
                         </div>
                         <div className="contact-info">{email}</div>
                     </div>
                     <div className="contact-field">
                         <div className="icon">
-                            <BsInstagram />
+                            <BsInstagram/>
                         </div>
-                        <div className="contact-info">{instagram}</div>
+                        <a href="https://www.instagram.com/flora.odontologia/">
+                            <div className="contact-info">{instagram}</div>
+                        </a>
                     </div>
                 </div>
             </div>
             <div className="column-image">
-                <img src={imageUrl} alt="Imagem" className="image_clinic" />
+                <img src={imageUrl} alt="Imagem" className="image_clinic"/>
             </div>
         </section>
     );
