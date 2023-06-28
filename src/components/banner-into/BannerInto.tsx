61f74bf91dd3a406134ed1673249b03cf275eff2
Guilherme Gonzaga <guilherme.gonzaga@eprecise.com.br>
2023-06-28 20:19:48 -0300

    fix: alteração na disposição de imagem em banner inicial / Correções no texto

--- src/components/banner-into/BannerInto.tsx
+++ src/components/banner-into/BannerInto.tsx
@@ -2,16 +2,17 @@ import './BannerInto.css';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import slide1 from '../../assets/slide1.svg';
+import bannerinto from '../../assets/bannerinto.svg';
+
 
 function BannerInto() {
     return (
             <div className='carousel-image'>
-                <img src={slide1} alt='Slide1' />
+                <img src={bannerinto} alt='Slide1' />
                 <div className='carousel-caption'>
                     <h1>Seu sorriso perfeito começa aqui!</h1>
-                    <h2>Conheça nossos serviços odontológicos de excelência,
-                        equipe especializada e tecnologia de ponta.</h2>
+                    <h2>Conheça nossos serviços odontológicos de excelência e nossa
+                        equipe especializada com tecnologia de ponta.</h2>
                 </div>
             </div>
     );
