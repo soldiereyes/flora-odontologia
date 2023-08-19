bb0dc3d4d69f10e70063f129b88046667c6e6eea
Guilherme Gonzaga <guilherme.gonzaga@eprecise.com.br>
2023-08-19 18:09:59 -0300

    fix: links de navegação na sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,15 +1,44 @@
 import './Sidebar.css';
 import {FiHome, FiMapPin, FiSettings, FiUser} from "react-icons/fi";
+import {Link} from "react-scroll";
 
-const Sidebar = ({isOpen}:any) => {
+const Sidebar = ({isOpen}: any) => {
     return (
         <div className={`sidebar ${isOpen ? 'open' : ''}`}>
 
             <div className="icon-container">
-                <FiHome />
-                <FiSettings />
-                <FiUser />
-                <FiMapPin />
+                <Link
+                    to={'section1'}
+                    spy={true}
+                    smooth={true}
+                    offset={-90}
+                    duration={700}>
+                    <FiHome/>
+                </Link>
+                <Link
+                    to={'section2'}
+                    spy={true}
+                    smooth={true}
+                    offset={-50}
+                    duration={1000}>
+                    <FiSettings/>
+                </Link>
+                <Link
+                    to={'section3'}
+                    spy={true}
+                    smooth={true}
+                    offset={-200}
+                    duration={1000}>
+                    <FiUser/>
+                </Link>
+                <Link
+                    to={'section4'}
+                    spy={true}
+                    smooth={true}
+                    offset={-50}
+                    duration={1000}>
+                    <FiMapPin/>
+                </Link>
             </div>
         </div>
     );
