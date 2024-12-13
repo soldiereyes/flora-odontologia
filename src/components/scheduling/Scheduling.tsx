f599e17303ab607ddb6ce893071f5bf152d68e4c
Seu Nome <seuemail@exemplo.com>
2024-12-13 14:44:26 -0300

    feat: muda a disposição da chamada para ação

--- src/components/scheduling/Scheduling.tsx
+++ src/components/scheduling/Scheduling.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import './Scheduling.css';
-import {BsWhatsapp} from "react-icons/bs";
 
 interface SchedulingProps {
-    title: string;
-    subtitle: string;
+    title?: string;
+    subtitle?: string;
 }
 
-const Scheduling: React.FC<SchedulingProps> = ({title,subtitle  }) => {
+const Scheduling: React.FC<SchedulingProps> = () => {
     return (
-        <div className="call-to-action">
-            <h2 className="title-call-to-action">{title}</h2>
-            <p className="subtitle-call-to-action">{subtitle}</p>
             <a href="https://wa.me/554891275624" className="button-call-to-action">
                 <i className="fab fa-whatsapp">
-                    <BsWhatsapp/>
-                </i> Agende pelo WhatsApp
+                </i> MARQUE UMA CONSULTA
             </a>
-        </div>
-
     )
 }
 export default Scheduling;
