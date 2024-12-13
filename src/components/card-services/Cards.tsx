6139591403e5798b7e85a9d8ea374d8a2aa6a6a9
Seu Nome <seuemail@exemplo.com>
2024-12-13 14:02:25 -0300

    feat: altera a disposição dos cards agora fixos e sem descrição

--- src/components/card-services/Cards.tsx
+++ src/components/card-services/Cards.tsx
@@ -1,22 +1,16 @@
-// Card.tsx
-
 import React from 'react';
 import './Cards.css';
 
 interface CardProps {
     title: string;
-    description: string;
     imageUrl: string;
 }
 
-const Card: React.FC<CardProps> = ({title, description, imageUrl}) => {
+const Card: React.FC<CardProps> = ({ title, imageUrl }) => {
     return (
-        <div className="slide">
+        <div className="card">
+            <img src={imageUrl} alt={title} />
             <h3>{title}</h3>
-            <p>{description}</p>
-            <div className='none'>
-                <img src={imageUrl} alt={title}/>
-            </div>
         </div>
     );
 };
